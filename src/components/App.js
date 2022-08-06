b81fefefb12024bdd86ee38873e826aba108807f
Pius <emefiene.pius@yahoo.com>
2022-08-06 00:50:26 -0500

    json fetched

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import FavoritesContainer from "./FavoritesContainer";
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/listings")
+    .then(res => res.json())
+    .then(data => {
+      setItems(data)
+    })
+
+  }, [])
+
+
   return (
     <div className="App">
      <h1>Hello World!</h1>
+     <FavoritesContainer data={data}/>
     </div>
   );
 }
---
+++ src/components/FavoritesCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FavoritesCard = ({data}) => {
+    console.log(data)
+  return (
+    <div>FavoritesCard</div>
+  )
+}
+
+export default FavoritesCard
