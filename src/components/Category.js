021cca9eae776e775a216b44b98895d234f37419
Pius <emefiene.pius@yahoo.com>
2022-08-07 00:53:26 -0500

    updated category, FavoritesContainer

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Category = () => {
+const Category = ({categoryStr}) => {
   return (
-    <div>Category</div>
+    <div>{categoryStr}</div>
   )
 }
 
--- src/components/FavoritesContainer.js
+++ src/components/FavoritesContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Category from './Category'
 import FavoritesCard from './FavoritesCard'
 
 const FavoritesContainer = ({items, categoryList}) => {
   return (
     <div>
+     {categoryList.map(categoryStr => <Category categoryStr={categoryStr} key={categoryStr.id}/>)}
      {items.map((itemsObj, index) => <FavoritesCard itemsObj={itemsObj} key={index}/>)}
-     
     </div>
   )
 }
